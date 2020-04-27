9f9949ab248df660c3b5231103c536f7fb1ac944
Sita Ram Kalluri <kalluriramkumar@gmail.com>
2020-04-27 13:48:53 +0530

    Initial Commit

---
+++ src/main/java/DemoClass.java
@@ -0,0 +1,12 @@
+public class DemoClass {
+
+    private int number1;
+    private int number2;
+
+    public int addNumbers(int arg1, int arg2){
+        int sum = arg1 + arg2;
+
+        return sum;
+    }
+
+}
---
+++ src/test/java/DemoClass_Test.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoClass_Test {
+
+    DemoClass demoClass = new DemoClass();
+
+
+    @Test
+    public void add_numbers_test() {
+
+        int expected_result = 10;
+
+        int actual_result = demoClass.addNumbers(5, 5);
+
+        assertEquals(expected_result, actual_result);
+    }
+
+    @Test
+    public void add_numbers_fail(){
+
+        int expected_result = 10;
+
+        int actual_result = demoClass.addNumbers(10, 10);
+
+        assertEquals(expected_result, actual_result);
+
+    }
+}
