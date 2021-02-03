0a380ecf4cc1fdb4a64d8ee770085f4b627cf7a1
sita ram kalluri <kalluriramkumar@gmail.com>
2021-02-03 16:16:54 +0530

    Fix unit test

--- src/test/java/NumberClass_Test.java
+++ src/test/java/NumberClass_Test.java
@@ -45,7 +45,7 @@ public class NumberClass_Test {
 
         int expected_result = 20;
 
-        int actual_result = numberClass.divideNumbers(40, 1);
+        int actual_result = numberClass.divideNumbers(40, 2);
 
         assertEquals(expected_result, actual_result);
 
